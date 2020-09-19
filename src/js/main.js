860ab201bc5b9a0215bbeeb1bef9508f7da8c27d
Angel Martinez <angel.codes139@gmail.com>
2020-09-18 21:22:40 -0500

    change the button

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,20 @@ const nav = document.querySelector('#navigation');
 btn.addEventListener('click', e => {
   if (nav.classList.contains('hidden')) {
     // Open nav
+
+    // Change the icon of the button
+    btn.querySelector('img').src = 'src/images/icon-close.svg';
     // Remove classes to the nav
     nav.classList.remove('hidden');
-
     // Display nav
     nav.classList.add('w-full', 'flex', 'flex-col', 'flex-grow');
   } else {
     // Close nav
+
+    // Change the icon of the button
+    btn.querySelector('img').src = 'src/images/icon-hamburger.svg';
     // Hidden the nav
     nav.classList.add('hidden');
-
     // Remove styles
     nav.classList.remove('w-full', 'flex', 'flex-col', 'flex-grow');
   }
